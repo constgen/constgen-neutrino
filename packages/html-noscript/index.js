2ffedfd36169c6eaca8b0b527f5e5e67f34ee5a5
constgen <const.gen@gmail.com>
2021-02-10 14:07:23 +0200

    Add default message to noscript

--- packages/html-noscript/index.js
+++ packages/html-noscript/index.js
@@ -2,9 +2,12 @@ let HtmlWebpackNoscriptPlugin = require('html-webpack-noscript-plugin')
 
 module.exports = function (innerHTML) {
 	return function (neutrino) {
+		const DEFAULT_MESSAGE = 'We’re sorry but our app doesn’t work properly without JavaScript enabled. Please enable it to continue.'
+		let message           = innerHTML || DEFAULT_MESSAGE
+
 		neutrino.config
 			.plugin('noscript')
-				.use(HtmlWebpackNoscriptPlugin, [innerHTML])
+				.use(HtmlWebpackNoscriptPlugin, [message])
 				.end()
 	}
 }
